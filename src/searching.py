0624f1e58ade4f93a3393d6fe297ac2a99985c0f
Brad Mortensen <b.mort86@gmail.com>
2019-03-27 16:34:59 -0600

    first pass solution for recursive binary search

--- src/searching.py
+++ src/searching.py
@@ -46,3 +46,9 @@ def binary_search_recursive(arr, target, low, high):
     if len(arr) == 0:
         return -1  # array empty
     # TO-DO: add missing if/else statements, recursive calls
+    elif target < arr[middle]:
+        return binary_search_recursive(arr, target, low, middle-1)
+    elif target > arr[middle]:
+        return binary_search_recursive(arr, target, middle+1, high) 
+    else:
+        return middle

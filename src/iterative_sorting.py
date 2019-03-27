f615fe657b2578bedd35cfcdee459e6d7513dcd6
Brad Mortensen <b.mort86@gmail.com>
2019-03-27 15:29:05 -0600

    finished count sort with help

--- src/iterative_sorting.py
+++ src/iterative_sorting.py
@@ -30,13 +30,29 @@ def bubble_sort(arr):
                 arr[j+1] = temp
     return arr
 
-
 arr = [23, 978906, 236, 27, 78, 8976, 252, 54, 7870]
 print(bubble_sort(arr))
 
 # STRETCH: implement the Count Sort function below
 
+# https://www.w3resource.com/python-exercises/data-structures-and-algorithms/python-search-and-sorting-exercise-10.php is how I got this weird sort :).
 
 def count_sort(arr, maximum=-1):
-
+    # Add one to the maximum input
+    max_key = maximum + 1
+    # Create a list with max+1 idicies all zero valued
+    count = [0] * max_key
+    # Count the occurances of each number and increment num at that index
+    for counts in array:
+        count[counts] += 1
+    # Set a counter variable to handle reordered array indicies
+    i = 0
+    # Loop range of max_key times for outer loop
+    for val in range(max_key):
+        # Loop through counting array and assign those values to
+        # [i]index of sorted array
+        for idx in range(count[val]):
+            arr[i] = val
+            # Increment counting var
+            i += 1
     return arr
